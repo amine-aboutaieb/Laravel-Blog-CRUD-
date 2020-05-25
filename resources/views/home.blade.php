2deef722daff7891b36ad351c2a87d83035aec21
Amine <amine.aboutaieb@gmail.com>
2020-05-25 12:34:19 +0100

    Final

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,16 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            
+            @foreach ($posts as $post)
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title">{{$post->title}}</h3>
+                        <p>{{$post->body}}</p>
+                        <small>{{$post->create_at}}</small>
+                    </div>
+                </div>
+                @include('inc.updateAndDel')
+            @endforeach
         </div>
     </div>
 </div>
