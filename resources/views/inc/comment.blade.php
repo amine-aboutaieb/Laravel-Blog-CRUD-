38ca139c6526a0880380d0652409308797640019
Amine <amine.aboutaieb@gmail.com>
2020-05-25 22:21:55 +0100

    Comment functionality(2)

--- resources/views/inc/comment.blade.php
+++ resources/views/inc/comment.blade.php
@@ -1,6 +1,8 @@
-{!! Form::open(['action'=>['CommentsController@addComment',$post->id], 'method'=>'POST']) !!}
+@if (Auth::check())
+    {!! Form::open(['action'=>['CommentsController@addComment',$post->id], 'method'=>'POST']) !!}
     <div class="my-5">
         {{ Form::text('comment_field','',['placeholder'=>'Comment...','autocomplete'=>'off']) }}
-        {{Form::submit('Comment')}}
+        {{Form::submit('Comment',['class'=>'btn btn-secondary'])}}
     </div>
-{!! Form::close() !!}
+    {!! Form::close() !!}
+@endif
